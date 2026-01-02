7d8416b14cb70d9a35c8181648c412dac5ee17cc
Likend <lyk2005@foxmail.com>
2026-01-03 00:02:48 +0800

    Feat: `opt::MemToReg::replaceOnceStore`

--- src/opt/MemToReg.cpp
+++ src/opt/MemToReg.cpp
@@ -1,26 +1,58 @@
 #include "opt/MemToReg.hpp"
 
 #include <unordered_map>
+#include <unordered_set>
 #include <utility>
+#include <vector>
 
 #include "ir/BasicBlock.hpp"
+#include "ir/Constants.hpp"
 #include "ir/Function.hpp"
 #include "ir/Instructions.hpp"
+#include "opt/MemToReg.hpp"
 #include "util/assert.hpp"
 
 using namespace opt;
 using namespace ir;
 
+void check(Function& f) {
+    std::unordered_set<Value*> values;
+
+    for (BasicBlock& bb : f) {
+        for (Instruction& inst : bb) {
+            values.insert(&inst);
+        }
+    }
+
+    for (BasicBlock& bb : f) {
+        for (auto& user : bb.users()) {
+            ASSERT(values.count(&user));
+        }
+        for (Instruction& inst : bb) {
+            for (auto& user : inst.users()) {
+                ASSERT(values.count(&user));
+            }
+        }
+    }
+}
+
 bool MemToRegPass::runOnFunction(Function& f) {
+    check(f);
+    if (f.empty()) return false;
+
     bool changed = false;
     for (BasicBlock& bb : f) {
-        changed |= replaceInnerBasicBlock(bb);
+        changed |= replaceInnerBlock(bb);
     }
+
+    check(f);
+
+    changed |= replaceOnceStore(f);
 
     return changed;
 }
 
-bool MemToRegPass::replaceInnerBasicBlock(BasicBlock& bb) {
+bool MemToRegPass::replaceInnerBlock(BasicBlock& bb) {
     enum State { LOAD, STORE };
     std::unordered_map<Value*, std::pair<Instruction*, State>> map;
 
@@ -58,6 +90,7 @@ bool MemToRegPass::replaceInnerBasicBlock(BasicBlock& bb) {
                     case LOAD:
                         break;
                     case STORE:
+                        prevInst->dropAllReferences();
                         bb.erase(prevInst);
                         changed = true;
                         break;
@@ -83,4 +116,63 @@ bool MemToRegPass::replaceInnerBasicBlock(BasicBlock& bb) {
         ++instIt;
     }
     return changed;
+}
+
+bool MemToRegPass::replaceOnceStore(Function& f) {
+    bool changed = false;
+
+    std::vector<AllocaInst*> allocas = getAllocas(f);
+
+    for (AllocaInst* alloca : allocas) {
+        std::vector<StoreInst*> stores;
+        std::vector<LoadInst*>  loads;
+
+        bool hasOtherUsage = false;
+        for (User& user : alloca->users()) {
+            if (auto* inst = dynamic_cast<StoreInst*>(&user))
+                stores.push_back(inst);
+            else if (auto* inst = dynamic_cast<LoadInst*>(&user))
+                loads.push_back(inst);
+            else {
+                hasOtherUsage = true;
+                break;
+            }
+        }
+
+        if (hasOtherUsage) continue;
+
+        Value* loadInstReplacement = nullptr;
+
+        if (stores.size() == 1) {
+            StoreInst* storeInst = stores[0];
+            ASSERT(storeInst->getPointerOperand() == alloca);
+            loadInstReplacement = storeInst->getValueOperand();
+            storeInst->dropAllReferences();
+            storeInst->parent()->erase(storeInst);
+        } else if (stores.size() == 0) {
+            loadInstReplacement =
+            PoisonValue::get(alloca->getAllocatedType());
+        }
+        if (loadInstReplacement) {
+            changed |= !loads.empty();
+            for (LoadInst* loadInst : loads) {
+                loadInst->replaceAllUsesWith(loadInstReplacement);
+                loadInst->dropAllReferences();
+                loadInst->parent()->erase(loadInst);
+            }
+            ASSERT(alloca->use_empty());
+            alloca->dropAllReferences();
+            alloca->parent()->erase(alloca);
+        }
+    }
+    return changed;
+}
+
+std::vector<AllocaInst*> MemToRegPass::getAllocas(Function& f) {
+    BasicBlock&              entry = f.front();
+    std::vector<AllocaInst*> allocas;
+    for (Instruction& inst : entry) {
+        if (auto* ai = dynamic_cast<AllocaInst*>(&inst)) allocas.push_back(ai);
+    }
+    return allocas;
 }
--- src/opt/MemToReg.hpp
+++ src/opt/MemToReg.hpp
@@ -1,13 +1,21 @@
 #pragma once
 
+#include <vector>
+
 #include "ir/BasicBlock.hpp"
+#include "ir/Function.hpp"
+#include "ir/Instructions.hpp"
 #include "ir/Pass.hpp"
+
 namespace opt {
 class MemToRegPass final : public ir::FunctionPass {
    public:
     bool runOnFunction(ir::Function&) override;
 
    private:
-    bool replaceInnerBasicBlock(ir::BasicBlock&);
+    bool replaceInnerBlock(ir::BasicBlock&);
+    bool replaceOnceStore(ir::Function&);
+
+    std::vector<ir::AllocaInst*> getAllocas(ir::Function&);
 };
 }  // namespace opt
